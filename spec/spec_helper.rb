fd2e2636b26c625d510618322bffbbb84c6fa207
Fizvlad <fizvlad@mail.ru>
2021-01-07 15:32:37 +0300

    feat: coverage and loading ENV variables for testing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+require 'simplecov'
+SimpleCov.start
+
 require 'vkponchik'
 
 RSpec.configure do |config|
@@ -12,4 +15,16 @@ RSpec.configure do |config|
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
+end
+
+require 'dotenv/load'
+
+# @return [String] spec group ID
+def vkponchik_group
+  Integer(ENV['VKPONCHIK_GROUP'], 10)
+end
+
+# @return [String] spec group API token
+def vkponchik_token
+  ENV['VKPONCHIK_TOKEN']
 end
